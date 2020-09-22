a2502aaf5d10dd6d4f127ccbed7725ea5ab02229
Hiba Machfej <hiba.mashfij@gmail.com>
2020-09-22 13:14:43 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,16 +1,1 @@
 // create your App component here
-// create your App component here
-import React, { Component } from 'react'; 
-
-
-class App extends Component { 
-    componentDidMount() {
-        fetch('http://api.open-notify.org/astros.json')
-          .then(response => response.json())
-          .then(data => {
-            console.log(data)
-          })
-      }
-    }
-     
-    export default App
